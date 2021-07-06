7868b0c8cadb8a6e2f658b773bd9c51628f28a9e
Ilnar Gilyaev <gilyaev.if@gmail.com>
2021-07-06 21:37:59 +0500

    Fix for remote start for tests

--- src/test/java/test/ProoftechItTests.java
+++ src/test/java/test/ProoftechItTests.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ProoftechItTests  {
+public class ProoftechItTests extends TestBase {
     PageElementsAndHelpers PageElementsAndHelpers = new PageElementsAndHelpers();
 
     static final String URL = "https://www.prooftechit.com/";
